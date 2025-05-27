bcaf0ca4b93f5fe0466d96d02fe1fee42ef2fc86
Nico Schulze <nico.schulze1602@gmail.com>
2025-05-27 21:20:18 +0200

    Second commit

--- app.py
+++ app.py
@@ -1,12 +1,17 @@
-from flask import Flask
+from flask import Flask, render_template
+import json
 
 app = Flask(__name__)
 
+with open('blog_posts.json', 'r') as file:
+    blog_posts = json.load(file)
+
 
 @app.route('/')
-def hello_world():
-    return 'Hello, World!'
+def index():
+    print("Hello There!")
+    return render_template('index.html', posts=blog_posts)
 
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    app.run(host="0.0.0.0", port=5001, debug=True)
