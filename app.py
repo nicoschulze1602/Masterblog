946bc071c0f6e566e127cc600e93d03cb537f4c6
Nico Schulze <nico.schulze1602@gmail.com>
2025-05-28 13:57:03 +0200

    Create an 'add.html' template-file for POST Requests and an @app.route to handle. I also implemented some helper functions to simplify the posting process and create a unique user id.

--- app.py
+++ app.py
@@ -1,16 +1,52 @@
-from flask import Flask, render_template
+import os.path
+
+from flask import Flask, render_template, request, redirect
 import json
 
 app = Flask(__name__)
+BLOGPOSTS = 'blog_posts.json'
 
-with open('blog_posts.json', 'r') as file:
-    blog_posts = json.load(file)
+
+def load_blogposts():
+    if not os.path.exists(BLOGPOSTS):
+        return []
+    with open(BLOGPOSTS, 'r', encoding='utf-8') as file:
+        return json.load(file)
+
+
+def save_blogposts(blogposts):
+    with open(BLOGPOSTS, 'w', encoding='utf-8') as file:
+        json.dump(blogposts, file, indent=4, ensure_ascii=False)
+
+
+def generate_user_id(blogposts):
+    if not blogposts:
+        return 1
+    return max(post["id"] for post in blogposts) + 1
 
 
 @app.route('/')
 def index():
-    print("Hello There!")
-    return render_template('index.html', posts=blog_posts)
+    blogposts = load_blogposts()
+    return render_template('index.html', posts=blogposts)
+
+
+@app.route('/add', methods=['GET', 'POST'])
+def add():
+    if request.method == 'POST':
+        blogposts = load_blogposts()
+
+        new_blogpost = {
+            "id": generate_user_id(blogposts),
+            "autor": request.form.get("author"),
+            "title": request.form.get("title"),
+            "content": request.form.get("content")
+        }
+        blogposts.append(new_blogpost)
+        save_blogposts(blogposts)
+        return redirect('/')
+
+    return render_template('add.html')
 
 
 if __name__ == '__main__':
